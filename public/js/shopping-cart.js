9200dac64a9e11a565b6119a3d4e17cfb15c0c29
tom474 <125862540+tom474@users.noreply.github.com>
2023-05-14 07:40:43 +0700

    update more customer page

---
+++ public/js/shopping-cart.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector("nav .container-lg button");
+const nav = document.querySelector("nav .container-lg .hamburger");
+
+btn.addEventListener("click", () => {
+    nav.classList.toggle("dropdown-nav");
+});
